bcba688a908b2327446b0ca5b69a6a4bb30debd1
Eric <emr@setd.fr>
2016-02-24 09:35:55 +0100

    First try at parsing the bs4 response

--- Tweet2Rss.py
+++ Tweet2Rss.py
@@ -2,15 +2,22 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def main():
-    with urllib.request.urlopen("https://twitter.com/CanardPcRedac") as f:
+__version__ = '0.1-SNAPSHOT'
+TWITTER_BASE_URL = 'https://twitter.com/'
+
+
+def parse(url):
+    with urllib.request.urlopen(TWITTER_BASE_URL + url) as f:
         response = f.read().decode('utf-8')
 
     html = response
     soup = BeautifulSoup(html, 'html.parser')
+    l = soup.findAll('p', attrs={'class': 'tweet-text'})
+    for text in l:
+        print(text)
 
 
 if __name__ == "__main__":
-    main()
+    parse('CanardPCRedac')
 
 
