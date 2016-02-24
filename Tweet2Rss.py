705affc1a02ed0f06926229948c9bdfef42518b0
Eric <emr@setd.fr>
2016-02-24 13:48:19 +0100

    Comments bla bla bla

--- Tweet2Rss.py
+++ Tweet2Rss.py
@@ -3,10 +3,15 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+"""
+Protoype of the web app
+"""
 
-__version__ = '0.1-SNAPSHOT'
-__author__ = "Eric"
-TWITTER_BASE_URL = 'https://twitter.com/'
+__author__ = "Eric \"Fistons\""
+__copyright__ = "Copyright 2016"
+__license__ = "MIT License"
+__version__ = "1.0-SNAPSHOT"
+__status__ = "prototype"
 
 
 class FuckingTweet:
@@ -24,15 +29,17 @@ class FuckingTweet:
         return "{} par {} [{}] le {}".format(self.tweet, self.author_name, self.author_account, self.date)
 
 
-class ShittyMain:
+class ShittyParser:
     """
     Shitty temporary main class
     """
+    TWITTER_BASE_URL = 'https://twitter.com/'
+
     def __init__(self):
         self.tweets = []
 
     def parse(self, twitter_account):
-        with urllib.request.urlopen(TWITTER_BASE_URL + twitter_account) as f:
+        with urllib.request.urlopen(ShittyParser.TWITTER_BASE_URL + twitter_account) as f:
             response = f.read().decode('utf-8')
 
         soup = BeautifulSoup(response, 'html.parser')
@@ -42,12 +49,11 @@ class ShittyMain:
             time = text.find('a', attrs={'class': 'tweet-timestamp'})['title']
             author = text.find('strong', attrs={'class': 'fullname'}).get_text()
             username = text.find('span', attrs={'class': 'username'}).get_text()
-
             self.tweets.append(FuckingTweet(tweet, time, author, username))
 
 
 if __name__ == "__main__":
-    m = ShittyMain()
+    m = ShittyParser()
     m.parse('CanardPCRedac')
     for t in m.tweets:
         print(t)
